a994b1bd4c52b9debca6e279a5805191aa73f6d9
GabrielResendel <codegabriel7@gmail.com>
2024-06-18 21:16:38 -0300

    comit inicial

---
+++ 1_LendoNaTela/atv1_lendo na tela.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lendoNaTela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alo mundo");
+        }
+    }
+}
+
---
+++ 2_NumDobro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumDobro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Write("Olá, digite um numero: ");
+            int num = Int32.Parse(Console.ReadLine());
+            string res = num.ToString();
+
+             Console.WriteLine($"O dobro de {num} é: {num * 2}");
+        }
+    }
+}
+
