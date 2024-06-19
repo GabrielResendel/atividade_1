6341b05801cd0ddb1b3ba44b915f0a7c3dd1fb2b
GabrielResendel <codegabriel7@gmail.com>
2024-06-18 22:11:10 -0300

    atv 3

--- 3_CentimetrosMetros/Program.cs
+++ 3_CentimetrosMetros/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("tadeu");
+﻿using System;
+
+namespace NumDobro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("----CONVERTENDO METROS EM CENTIMETROS----");
+          Console.Write("digite o numero de metros que deseja converter: ");
+            double metros = Convert.ToDouble(Console.ReadLine());
+
+              Console.WriteLine($"A quantidade de {metros} metros covertida em centimetros é: {metros * 100}");
+        }
+    }
+}
+
