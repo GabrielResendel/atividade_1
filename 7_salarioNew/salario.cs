2e917f4fbff3129f1dbd489421074c9e864710f6
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 10:21:06 -0300

    atv 7

---
+++ 7_salarioNew/salario.cs
@@ -0,0 +1,18 @@
+﻿// 7. Escreva um algoritmo para ler o salário mensal atual de um funcionário e calcular e escrever o valor do novo salário com 12% de reajuste. 
+using System;
+
+namespace AntecessorSucessor
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu salario mensal: ");
+               double salario = Convert.ToDouble(Console.ReadLine());
+               double reajuste = salario * 0.12;
+               double NovoSalario = salario + reajuste;
+
+            Console.WriteLine($"Seu salario é: {salario} com o reajuste de 12% ele fica: {NovoSalario}");
+        }
+    }
+}
