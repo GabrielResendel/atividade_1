d9036ca59fb15ddc797a91e13819a0d9cc14e32a
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 10:30:00 -0300

    desafio1

---
+++ Exercicio DESAFIOS (Ao infinito e além)/1_ConversaoParaMinutos/ConvertMin.cs
@@ -0,0 +1,29 @@
+﻿//1. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos. (1 minutos tem quantos segundos? 1 hora tem quantos minutos?)
+using System;
+
+namespace ConvertMinutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Digite a quantidade de dias: ");
+            int dias = Convert.ToInt32(Console.ReadLine());
+           
+            Console.Write("Digite a quantidade de horas: ");
+            int horas = Convert.ToInt32(Console.ReadLine());
+         
+            Console.Write("Digite a quantidade de minutos: ");
+            int minutos = Convert.ToInt32(Console.ReadLine());
+    
+            Console.Write("Digite a quantidade de segundos: ");
+            int segundos = Convert.ToInt32(Console.ReadLine());
+            
+            int totalSegundos = (dias * 24 * 3600) + (horas * 3600) + (minutos * 60) + segundos;
+
+            Console.WriteLine("O total em segundos é: " + totalSegundos);
+        }
+    }
+}
+
