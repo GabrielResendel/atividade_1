a6f2299a41bdb2e2562a8126e68f84f688a739c9
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 09:50:16 -0300

    atv 4

---
+++ 4_AreaQuadrado/LerDimensãoQuadrado.cs
@@ -0,0 +1,20 @@
+﻿// 4. Escreva um algoritmo para ler as dimensões de um quadrado e mostre sua área (base * altura ou base ).
+using System;
+
+namespace LerDimensão
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho de um lado do quadrado: ");
+                double lado = Convert.ToDouble(Console.ReadLine());
+
+                double dimen = lado * lado;
+
+                Console.WriteLine("A dimensão do quadrado e: " + dimen);
+
+
+        }
+    }
+}
