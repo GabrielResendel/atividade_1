615c0150c35a7ecbf64abc2a8d1f34830c4a680a
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 10:51:22 -0300

    desafio 2

---
+++ Exercicio DESAFIOS (Ao infinito e além)/2_ConversorDeGraus/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2.Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo): 
+
+using System;
+
+namespace ConversorDeGraus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero de graus Fahrenheit que deseja converter para graus Celsius: ");
+            double GrauF = Convert.ToDouble(Console.ReadLine());
+            double convert = GrauF - 32;
+
+            Console.WriteLine($"A conversão de {GrauF} Fahrenheit para graus Celsius e de : {convert * 5/9}  ");
+        }
+    }
+}
