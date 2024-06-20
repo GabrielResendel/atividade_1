51d092c20da0f6e0dd4f81db5a6fe53081a96feb
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 10:00:15 -0300

    atv 5

---
+++ 5_AreaCirculo/AreaCirculo.cs
@@ -0,0 +1,18 @@
+﻿
+// 5. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área (area do círculo = 3.14 * raio ao quadrado).
+using System;
+
+namespace AreaCirculo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o raio do circulo: ");
+                double raio = Convert.ToDouble(Console.ReadLine());
+                double area = 3.14 * Math.Pow(raio, 2);
+
+                Console.WriteLine("A area do circulo e de: " + area);
+        }
+    }
+}
