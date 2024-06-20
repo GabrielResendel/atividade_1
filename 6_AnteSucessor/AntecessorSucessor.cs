87e3842ce5040aec3b92c01d551ca779f43dc9cc
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 10:09:07 -0300

    atv 6

---
+++ 6_AnteSucessor/AntecessorSucessor.cs
@@ -0,0 +1,21 @@
+﻿// 6.Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o valor, seu antecessor e seu sucessor (cada um em uma linha).
+
+using System;
+
+namespace AntecessorSucessor
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+               int num = Convert.ToInt32(Console.ReadLine());
+                int ante = num - 1;
+                int suce = num + 1;
+
+                Console.WriteLine("Seu numero e: " + num);
+                Console.WriteLine("Seu antecessor e: " + ante);
+                Console.WriteLine("Seu sucessor e: " + suce);
+        }
+    }
+}
