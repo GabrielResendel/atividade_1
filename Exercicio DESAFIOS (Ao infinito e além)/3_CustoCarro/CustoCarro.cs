4ab59434259e9c55d47ddfeff13954ffe734f3f6
GabrielResendel <codegabriel7@gmail.com>
2024-06-20 11:09:51 -0300

    desafio 3

---
+++ Exercicio DESAFIOS (Ao infinito e além)/3_CustoCarro/CustoCarro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustoCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de custo de fabrica do carro: ");
+            double fabrica = Convert.ToDouble(Console.ReadLine());
+            double SomaDistribuidor = fabrica * 0.28;
+            double SomaImposto = fabrica * 0.45;
+            double ValorFinal = fabrica + SomaImposto + SomaImposto;
+
+            Console.WriteLine($"o valor final do carro ao consumidor e: {ValorFinal}");
+        }
+    }
+}
